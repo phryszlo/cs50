b2cf55095067040fa94a3de1bac906b83a82e543
Daren Porter <dporter@sbmgroup.local>
2020-07-09 12:12:13 -0400

    tasks pretty ready. we need to learn a deployment chain thingy whatchacaller

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,12 +1,13 @@
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 
-tasks = ["foo","bar","bas"]
-
+tasks = []
 class NewTaskForm(forms.Form):
   task = forms.CharField(label="New Task")
-  priority = forms.IntegerField(label="Priority", \
-    min_value=1, max_value=5)
+  # priority = forms.IntegerField(label="Priority", \
+  #   min_value=1, max_value=5)
 
 def index(request):
   return render(request, "tasks/index.html", {
@@ -19,6 +20,7 @@ def add(request):
     if form.is_valid():
       task = form.cleaned_data["task"]
       tasks.append(task)
+      return HttpResponseRedirect(reverse("tasks:index"))
     else:
       return render(request, "tasks/add.html", {
         "form": form
