4cab7244aa49dc2ea2ae48ebf0952753c156ef53
Daren Porter <dporter@sbmgroup.local>
2020-07-09 00:36:33 -0400

    tasks css underway, pushing for digital ocean test

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -5,7 +5,8 @@ tasks = ["foo","bar","bas"]
 
 class NewTaskForm(forms.Form):
   task = forms.CharField(label="New Task")
-  priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+  priority = forms.IntegerField(label="Priority", \
+    min_value=1, max_value=5)
 
 def index(request):
   return render(request, "tasks/index.html", {
